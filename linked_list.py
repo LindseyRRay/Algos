c0e1642f46c60a788b04b8486903e37f52b2a7fe
lrraymond13 <lrraymond13@gmail.com>
2014-10-28 15:27:03 -0400

    WIP for linked lists

---
+++ linked_list.py
@@ -0,0 +1,124 @@
+#Linked List in Python
+#Implemented as a class with next and previous data attributes and several methods
+import pdb
+
+class Node(object):
+	def __init__(self, node_data):
+		self.data = node_data
+		self.next = None
+		self.previous = None
+
+	def getData(self):
+		print(self.data)
+		return self.data
+
+	def getNext(self):
+		return self.next
+
+	def setData(self, newData):
+		self.data = newData
+
+	def setNext(self, next):
+		self.next = next
+
+	def getPrev(self):
+		return self.previous
+
+	def setData(self, newData):
+		self.previous = newData
+
+class Linked_List(object):
+	def __init__(self):
+		self.head = None
+
+	def isEmpty(self):
+		return self.head == None
+
+	def add(self, new_node_data):
+		''' adds a new node to the beginning of the list''' 
+		new_node = Node(new_node_data)
+		new_node.setNext(self.head)
+		self.head = new_node
+		if self.head != None:
+			self.head.previous = new_node
+			
+
+
+	def size(self):
+		'''counts number of nodes in the list '''
+		next = self.head
+		size = 0
+		while next != None:
+			size += 1
+			next = next.getNext()
+		return size
+
+	def search(self, search_value):
+		'''finds a specific value in the list '''
+		found = False
+		next = self.head
+		count = 0
+		while next.next != None and next.data != search_value:
+			count += 1
+			next = next.getNext()
+		return next, count
+
+	def remove(self, remove_value):
+		'''removes specified value from the linked list '''
+		current = self.head
+		remove = False
+
+		future = current.getNext()
+
+		while not remove:
+			if current.data == remove_value:
+				remove = True
+				if current.previous == None:
+					self.head = current.getNext()
+				else:
+					current.previous.setNext(current.getNext())
+					current.getNext().previous = current.previous
+			else:
+				current = current.getNext()
+
+	def insert(self, new_item):
+		'''adds a new item into a specific position'''
+		temp = self.index(new_item)
+		new_item.previous = temp.previous
+		new_item.next = temp.next
+		temp.previous = new_item
+		temp.previous.next = new_item
+
+
+
+	def index(self, count):
+		''' returns node of an index in linked list with 0 being the head '''
+		start = self.head
+		while count > 0:
+			start = start.getNext()
+			count -= 1
+		if count < self.size():
+			return start
+		else:
+			raise ValueError
+
+
+
+
+if __name__ == '__main__':
+	llist = Linked_List()
+	test = Node(100)
+	test2 = Node(200)
+	print(test.data)
+	llist.add(test)
+	#pdb.set_trace()
+
+	assert llist.size() ==1, "Size of list Calculating Incorrectly"
+	llist.add(test2)
+	llist.head.getData()
+	test2.getData()
+	print(llist.head.data)
+
+
+
+
